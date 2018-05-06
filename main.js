f5f72bb317680368053dd039c99e9337cbb765ca
Tim Thuis <timthuis@MacBook-Pro-van-Tim.local>
2018-05-06 11:12:42 +0200

    add def title animation

--- main.js
+++ main.js
@@ -5,19 +5,21 @@ var docWidth = document.body.offsetWidth;
 var docHeight = document.body.offsetHeight;
 var titleElement = document.querySelector("#title h1");
 
-introTl.staggerFrom("#title span", 1, {
+document.addEventListener("mousemove", mouseMove);
+
+introTl.staggerFrom("#title div", 1, {
+  y: "-35%",
   opacity: 0,
 }, 0.5, "start")
-.from("#title span", 2, {
+.from("#title div", 2, {
     textShadow: "0px 0px 0px #e8e8e8, 0 0 0 #000000",
 }, "start+=2");
 
-document.addEventListener("mousemove", mouseMove);
 
 function mouseMove(event) {
-  mouseLocationX = Math.round((event.clientY - (docWidth / 2)) / docWidth * 15);
-  mouseLocationY = Math.round((event.clientX - (docHeight / 2)) / docHeight * 15);
-  // console.log(mouseLocationX);
-  // console.log(mouseLocationY);
-  titleElement.style.transform = `rotateX(${mouseLocationX}deg) rotateY(${mouseLocationY}deg) translateZ(0px)`;
+  mouseLocationX = Math.round((event.clientX - (docWidth / 2)) / docWidth * 100);
+  mouseLocationY = Math.round((event.clientY - (docHeight / 2)) / docHeight * 100);
+  // console.log(-mouseLocationX);
+  // titleElement.style.transform = `rotateX(${mouseLocationY}deg) rotateY(${-mouseLocationX}deg) translateZ(0px)`;
+  // titleElement.style.transform = `rotateY(${mouseLocationX}deg)`;
 }
