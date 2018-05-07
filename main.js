c391e99dcfd73ce9020384924a0ca798721cd8b5
Tim Thuis <timthuis@MacBook-Pro-van-Tim.local>
2018-05-07 09:41:10 +0200

    Add def intro animation

--- main.js
+++ main.js
@@ -7,18 +7,23 @@ var titleElement = document.querySelector("#title h1");
 
 document.addEventListener("mousemove", mouseMove);
 
-introTl.staggerFrom("h1 div", 1, {
+introTl.from("#cube", 2, {
+  scale: 0,
+  ease: Elastic.easeOut.config(0.75, 0.5),
+  opacity: 0,
+}, "start")
+.staggerFrom("h1 div", 1, {
   y: "-35%",
   opacity: 0,
 }, 0.5, "start")
-.from("h1 div", 2, {
+.from("h1 div", 0.75, {
     textShadow: "0px 0px 0px #e8e8e8, 0 0 0 #000000",
-}, "start+=2");
+}, "start+=1.5");
 
 
 function mouseMove(event) {
-  mouseLocationX = Math.round((event.clientX - (docWidth / 2)) / docWidth * 100);
-  mouseLocationY = Math.round((event.clientY - (docHeight / 2)) / docHeight * 100);
+  mouseLocationX = Math.round((event.clientX - (docWidth / 2)) / docWidth * 20);
+  mouseLocationY = Math.round((event.clientY - (docHeight / 2)) / docHeight * 20);
   // console.log(-mouseLocationX);
   // titleElement.style.transform = `rotateX(${mouseLocationY}deg) rotateY(${-mouseLocationX}deg) translateZ(0px)`;
   // titleElement.style.transform = `rotateY(${mouseLocationX}deg)`;
