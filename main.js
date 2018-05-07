57da6949118c4174d008f4eaf0b927656f76478d
Tim Thuis <timthuis@MacBook-Pro-van-Tim.local>
2018-05-07 10:03:57 +0200

    add mouse tracking to title

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var introTl = new TimelineMax();
 var mouseLocationX;
 var mouseLocationY;
+var xMovement;
+var yMovement;
+var velocity = 0.03;
 var docWidth = document.body.offsetWidth;
 var docHeight = document.body.offsetHeight;
 var titleElement = document.querySelector("#title h1");
@@ -22,9 +25,12 @@ introTl.from("#cube", 2, {
 
 
 function mouseMove(event) {
-  mouseLocationX = Math.round((event.clientX - (docWidth / 2)) / docWidth * 20);
-  mouseLocationY = Math.round((event.clientY - (docHeight / 2)) / docHeight * 20);
-  // console.log(-mouseLocationX);
-  // titleElement.style.transform = `rotateX(${mouseLocationY}deg) rotateY(${-mouseLocationX}deg) translateZ(0px)`;
-  // titleElement.style.transform = `rotateY(${mouseLocationX}deg)`;
+  mouseLocationX = (event.clientX - (docWidth / 2)) / docWidth * 100;
+  mouseLocationY = (event.clientY - (docHeight / 2)) / docHeight * 100;
+  // console.log("x: " + -mouseLocationX + ",y: " + mouseLocationY);
+
+  xMovement = mouseLocationX * velocity;
+  yMovement = mouseLocationY * velocity;
+  // console.log("x: " + xMovement + ",y: " + yMovement);
+  titleElement.style.transform = "rotateX(" + -yMovement + "deg) rotateY(" + xMovement + "deg) translateZ(0px)";
 }
